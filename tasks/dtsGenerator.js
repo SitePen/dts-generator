16a86fc3cb3e06c8ebdf7884a9f4c4542101fe60
Kitson Kelly <me@kitsonkelly.com>
2015-11-03 13:32:17 +0000

    Fix grunt task. Fixes #43.

--- tasks/dtsGenerator.js
+++ tasks/dtsGenerator.js
@@ -1,17 +1,17 @@
 module.exports = function (grunt) {
-	var dtsGenerator = require('../index');
+	var dtsGenerator = require('../index').default;
 	var path = require('path');
 
 	grunt.registerMultiTask('dtsGenerator', function () {
 		var done = this.async();
-		var onProgress = grunt.verbose.writeln.bind(grunt.verbose);
 
 		var kwArgs = this.options();
+		kwArgs.sendMessage = grunt.verbose.writeln.bind(grunt.verbose);
 		kwArgs.files = this.filesSrc.map(function (filename) {
 			return path.relative(kwArgs.baseDir, filename);
 		});
 
-		dtsGenerator.generate(kwArgs, onProgress).then(function () {
+		dtsGenerator(kwArgs).then(function () {
 			grunt.log.writeln('Generated d.ts bundle at \x1b[36m' + kwArgs.out + '\x1b[39;49m');
 			done();
 		}, done);
