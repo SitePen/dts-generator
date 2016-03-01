f14e636dc7bfa31b88f4728f4cab1b3d066872eb
Kitson Kelly <me@kitsonkelly.com>
2016-03-01 09:10:39 +0000

    Clean up packaging and typings

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,5 +1,4 @@
 /// <reference path="glob/glob.d.ts" />
-/// <reference path="minimatch/minimatch.d.ts" />
 /// <reference path="bluebird/bluebird.d.ts" />
 /// <reference path="mkdirp/mkdirp.d.ts" />
 /// <reference path="../node_modules/typescript/lib/typescript.d.ts"/>
