61b1c9befbc4a4417203aa198d49e6c815fed4cb
David Goldstein <dgoldstein0@gmail.com>
2017-01-12 06:46:03 -0800

    Move compilation to use tsconfig.json files instead of hardcoded command lines (#88)

--- tests/run.sh
+++ tests/run.sh
@@ -4,8 +4,9 @@ cd "$(dirname $0)/.."
 echo "Linting files..."
 node_modules/.bin/tslint --config tslint.json index.ts bin/dts-generator.ts tests/intern.ts tests/unit/all.ts
 echo "Building modules..."
-node_modules/.bin/tsc --module umd --target es5 --moduleResolution node node_modules/intern/typings/intern/intern.d.ts typings/tsd.d.ts tests/intern.ts tests/unit/all.ts
-node_modules/.bin/tsc --module umd --target es5 --sourcemap --moduleResolution node typings/tsd.d.ts index.ts bin/dts-generator.ts
+node_modules/.bin/tsc --project tsconfig.json
+echo "Building tests..."
+node_modules/.bin/tsc --project tests/tsconfig.json
 echo "Running intern..."
 node_modules/.bin/intern-client config=tests/intern reporters=Console
 echo "Cleanup..."
